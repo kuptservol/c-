79dbc63dc9bec137c2e6d16b1546862aefd568f3
Sergey Kuptsov <kuptservol@yandex-team.ru>
2019-09-02 23:26:30 +0300

    * update fields

---
+++ solutions/red/update_fields/airline_ticket.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <string>
+
+using namespace std;
+
+struct Date {
+    int year, month, day;
+};
+
+struct Time {
+    int hours, minutes;
+};
+
+
+bool operator<(const Date &date1, const Date &date2);
+bool operator!=(const Date &date1, const Date &date2);
+bool operator==(const Date &date1, const Date &date2);
+bool operator<(const Time &time1, const Time &time2);
+ostream& operator<<(ostream& os, const Date& date);
+istream &operator>>(istream &stream, Date &date);
+istream &operator>>(istream &stream, Time &time);
+
+struct AirlineTicket {
+    string from;
+    string to;
+    string airline;
+    Date departure_date;
+    Time departure_time;
+    Date arrival_date;
+    Time arrival_time;
+    int price;
+};
